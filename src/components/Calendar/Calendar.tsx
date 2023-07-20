2e46c1b48a0882dcf8a5478ae9eee5b2145740f3
Harsh97x <Rynzie@protonmail.com>
2023-07-20 15:30:49 +0530

    added components

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -3,9 +3,19 @@ import ReactCalendar from "react-calendar";
 
 interface indexProps {}
 
+interface Datetype {
+  justDate: Date | null;
+  dateTime: Date | null;
+}
+
 const index: FC<indexProps> = ({}) => {
+  const [date, setDate] = useState<Datetype>({
+    justDate: null,
+    dateTime: null,
+  });
+
   return (
-    <div>
+    <div className="flex h-screen flex-col items-center justify-center">
       <ReactCalendar
         minDate={new Date()}
         className="REACT-CALENDAR, p-2"
